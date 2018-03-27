1b19f093cc60c340971825396b6644d31fdeb935
Further Development <furtherdev1@furthers-imac.lan>
2018-03-27 12:59:25 -0700

    add postcss: autoprefixer, pixrem, normalize

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,20 @@
+const autoprefixer = require('autoprefixer')
+const pixrem = require('pixrem')
+
 module.exports = {
-  plugins: ['gatsby-plugin-react-helmet']
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-postcss-sass',
+      options: {
+        postCssPlugins: [
+          // pixrem(),
+          autoprefixer({
+            browsers: ['last 2 versions']
+          })
+        ]
+      },
+      precision: 8
+    }
+  ]
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,18 +1,16 @@
+import '../../node_modules/normalize.css/normalize.css'
 import React, { Component } from 'react'
 import Helmet from 'react-helmet'
+
+import styles from './index.module.css'
+console.log(styles)
 
 export default ({ children }) => (
   <div>
     <Helmet 
       title="Further Gatsby Starter"
     />
-    <div 
-      style={{
-        margin: '0  auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45em',
-        paddingTop: 0,
-      }}>
+    <div className={styles.container}>
       {children()}
     </div>
   </div>
