fbdab0e8a78fad3763b68677ac3847b6e11e7ce0
Further Development <furtherdev1@furthers-imac.lan>
2018-03-27 17:14:13 -0700

    tailwind works!

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,23 +3,23 @@ const autoprefixer = require('autoprefixer')
 module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-postcss-sass',
-      options: {
-        postCssPlugins: [
-          require('postcss-easy-import'),
-          autoprefixer({
-            browsers: ['last 2 versions']
-          })
-        ]
-      },
-      precision: 8
-    },
-    {
-      resolve: 'gatsby-plugin-typography',
-      options: {
-        pathToConfigModule: 'src/utils/typography.js'
-      }
-    }
+    // {
+    //   resolve: 'gatsby-plugin-postcss-sass',
+    //   options: {
+    //     postCssPlugins: [
+    //       require('postcss-easy-import'),
+    //       autoprefixer({
+    //         browsers: ['last 2 versions']
+    //       })
+    //     ]
+    //   },
+    //   precision: 8
+    // },
+    // {
+    //   resolve: 'gatsby-plugin-typography',
+    //   options: {
+    //     pathToConfigModule: 'src/utils/typography.js'
+    //   }
+    // }
   ]
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 
-import styles from './index.module.css'
-console.log(styles)
+import './index.css'
 
 export default ({ children }) => (
-  <div>
-    <Helmet 
-      title="Further Gatsby Starter"
-    />
-    <div className={styles.container}>
-      {children()}
+  <div className="flex flex-col font-sans min-h-screen text-grey-darkest">
+    <div>
+      <Helmet 
+        title="Further Gatsby Starter"
+      />
+      <div className="flex flex-col flex-1 md:justify-center max-w-xl mx-auto px-4 py-8 md:p-8 w-full">
+        {children()}
+      </div>
     </div>
   </div>
 )
