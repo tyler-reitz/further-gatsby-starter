1232103d28488c85937870cfe40629ad34ca3bf8
Further Development <furtherdev1@furthers-imac.lan>
2018-03-28 12:49:55 -0700

    fixes Card component width on small screens

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 export default () =>(
-  <div className="flex-none max-w-xs rounded overflow-hidden shadow-lg mx-2 mb-8">
+  <div className="flex-shrink max-w-xs rounded overflow-hidden shadow-lg mx-2 mb-8">
     <img className="w-full" src="http://via.placeholder.com/320x195" alt="Sunset in the mountains" />
     <div className="px-6 py-6">
       <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
