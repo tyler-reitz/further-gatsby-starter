78182e75d5960abda1c9ba71390af4c57d08ce89
Further Development <furtherdev1@furthers-imac.lan>
2018-03-28 11:33:15 -0700

    tweak some widths

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -4,7 +4,7 @@ import Button from '../Button'
 
 export default () => (
    <div className="flex bg-grey-lighter">
-      <div className="max-w-xl sm:w-5/6 md:w-2/3 mx-auto p-4 sm:p-8 md:my-8">
+      <div className="max-w-xl w-5/6  mx-auto p-4 sm:p-8 md:my-8">
         <h2 className="leading-loose">Further Gatsby Starter</h2>
         <p className="mb-4 leading-normal">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur quia totam ut commodi esse, excepturi consectetur harum asperiores recusandae deleniti nihil distinctio optio assumenda provident quae? Quos laudantium officiis earum.</p>
         <Button>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Card from '../components/Card'
 const IndexPage = () => (
   <div>
     <Hero />
-    <div className="flex flex-wrap justify-center max-w-2xl mx-auto py-8">
+    <div className="flex flex-wrap justify-center max-w-2xl mx-auto py-8  md:my-8">
       { [1,2,3].map((_, idx) => <Card key={idx} />) }
     </div>
   </div>
