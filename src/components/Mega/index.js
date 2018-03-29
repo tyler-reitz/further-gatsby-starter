a5ed650cb144eed09e83420876490644d5b45bbe
Further Development <furtherdev1@furthers-imac.lan>
2018-03-29 15:12:51 -0700

    rc-mega-menu hacked to work

--- src/components/Mega/index.js
+++ src/components/Mega/index.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
-
-const services = ['A', 'B', 'C', 'D', 'E', 'F']
+import Menu, { SubMenu, Item as MenuItem, Divider } from 'rc-menu'
 
 export default () => (
   <nav id="mega" className="bg-grey-darker text-white hidden md:block">
-    <ul className="flex flex-col sm:flex-row justify-between list-reset max-w-2xl mx-auto px-8">
-      { services.map(name => 
-          <li className="p-6 text-grey cursor-pointer text-sm" key={name}>{`Service ${name}`}</li>) 
-      }
-    </ul>
+    <Menu mode="horizontal" className="flex justify-center list-reset leading-loose">
+      <MenuItem>Home</MenuItem>
+      <MenuItem>About</MenuItem>
+      <MenuItem>Contact</MenuItem>
+      <SubMenu key="1" title="Submenu 1" className="bg-grey-darker text-grey-lighter">
+        <MenuItem key="1-1">item1-1</MenuItem>
+        <MenuItem key="1-2">item1-2</MenuItem>
+      </SubMenu>
+      <SubMenu key="2" title="Submenu 2" className="bg-grey-darker text-grey-lighter">
+        <MenuItem key="2-1">item2-1</MenuItem>
+        <MenuItem key="2-2">item2-2</MenuItem>
+      </SubMenu>
+    </Menu>
   </nav>
 )
