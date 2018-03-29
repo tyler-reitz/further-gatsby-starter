936f102dc10eb4509a34f044298ca67038096f81
Further Development <furtherdev1@furthers-imac.lan>
2018-03-28 17:39:41 -0700

    collapses mobile-menu > 768

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,26 @@
-import React from "react"
+import React, { Component } from "react"
 import Link from "gatsby-link"
 
 import Mega from '../Mega'
 
-const Header = () => {
-  const handleClick = (e) => {
-    e.preventDefault();    
+class Header extends Component {
+
+  componentDidMount() {
+    window.addEventListener('resize', (e) => {
+      const { innerWidth } = window
+
+      if (innerWidth > 768) {
+        const main = document.getElementById('main')
+        const mobileMenu = document.getElementById('mobile-menu')
+
+        main.style.transform = ""
+        setTimeout(() => mobileMenu.style.display = "none", 300)
+      }
+    })
+  }
+
+  handleClick = (e) => {
+    e.preventDefault();
     const main = document.getElementById('main')
     const mobileMenu = document.getElementById('mobile-menu')
 
@@ -19,69 +34,71 @@ const Header = () => {
     setTimeout(() => mobileMenu.style.display = "none", 300)
   };
 
-  return (
-    <nav className="bg-grey ">
-      <div className="flex flex-wrap items-center justify-between max-w-2xl mx-auto p-4 md:p-8">
-        <Link to="/" className="flex items-center no-underline text-white">
-          <svg
-            className="fill-current hx1-8 mr-2 w-8"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            xmlns="http://www.w3.org/2000/svg"
+  render() {
+    return (
+      <nav className="bg-grey ">
+        <div className="flex flex-wrap items-center justify-between max-w-2xl mx-auto p-4 md:p-8">
+          <Link to="/" className="flex items-center no-underline text-white">
+            <svg
+              className="fill-current hx1-8 mr-2 w-8"
+              width="54"
+              height="54"
+              viewBox="0 0 54 54"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+            </svg>
+            <span className="hidden sm:block font-bold text-xl tracking-tight">
+              Further
+            </span>
+          </Link>
+
+          <button
+            className="block md:hidden border border-white flex items-center px-3 py-2 rounded text-white"
+            onClick={this.handleClick}
           >
-            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-          </svg>
-          <span className="hidden sm:block font-bold text-xl tracking-tight">
-            Further
-          </span>
-        </Link>
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
 
-        <button
-          className="block md:hidden border border-white flex items-center px-3 py-2 rounded text-white"
-          onClick={handleClick}
-        >
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+          <div
+            id="nav"
+            className="hidden md:flex md:items-center w-full md:w-auto"
           >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
+            <div className="text-sm">
+              <Link
+                to="/"
+                className="block mt-4 md:inline-block md:mt-0 mr-6 no-underline text-white"
+              >
+                Home
+              </Link>
 
-        <div
-          id="nav"
-          className="hidden md:flex md:items-center w-full md:w-auto"
-        >
-          <div className="text-sm">
-            <Link
-              to="/"
-              className="block mt-4 md:inline-block md:mt-0 mr-6 no-underline text-white"
-            >
-              Home
-            </Link>
+              <Link
+                to="/about"
+                className="block md:inline-block mt-4 md:mt-0 mr-6 no-underline text-white"
+              >
+                About
+              </Link>
 
-            <Link
-              to="/about"
-              className="block md:inline-block mt-4 md:mt-0 mr-6 no-underline text-white"
-            >
-              About
-            </Link>
-
-            <Link
-              to="/contact"
-              className="block md:inline-block mt-4 md:mt-0 no-underline text-white"
-            >
-              Contact
-            </Link>
+              <Link
+                to="/contact"
+                className="block md:inline-block mt-4 md:mt-0 no-underline text-white"
+              >
+                Contact
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-      <Mega />
-    </nav>
-  );
-};
+        <Mega />
+      </nav>
+    )
+  }
+}
 
 export default Header;
