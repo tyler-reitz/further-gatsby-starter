9ab4d54a0fcd639e56714835b94c04bfb23b59dc
Further Development <furtherdev1@furthers-imac.lan>
2018-03-29 13:47:34 -0700

    rc-menu working; rc-menu styles reset

--- src/components/MobileMenu/index.js
+++ src/components/MobileMenu/index.js
@@ -1,30 +1,27 @@
 import React, { Component } from 'react'
 import Link from 'gatsby-link'
-import Menu, { SubMenu, MenuItem } from 'rc-menu'
-
+import Menu, { SubMenu, Item as MenuItem } from 'rc-menu'
 
 class MobileMenu extends Component {
-
-  renderMenuItem = title => (
-    <MenuItem key={title} className="leading-loose ">
-      <Link className="no-underline text-grey capitalize" to={`/${title}`}>
-        {title}
-      </Link>
-    </MenuItem>
-  )
-
   render() {
-    const { renderMenuItem } = this
-
     return (
       <div id="mobile-menu" className="bg-grey-darkest absolute text-grey pin-t w-4/5 h-screen">
-        <Menu className="list-reset p-4">
-          {
-            ['home', 'about', 'contact']
-              .map(item => 
-                renderMenuItem(item)
-              )
-          }
+        <Menu mode="inline" selectable={false} className="list-reset leading-loose">
+          <MenuItem>Home</MenuItem>
+          <MenuItem>About</MenuItem>
+          <MenuItem>Contact</MenuItem>
+          <SubMenu key="1" title="Submenu 1">
+            <MenuItem key="1-1">item1-1</MenuItem>
+            <MenuItem key="1-2">item1-2</MenuItem>
+          </SubMenu>
+          <SubMenu key="2" title="Submenu 2">
+            <MenuItem key="2-1">item2-1</MenuItem>
+            <MenuItem key="2-2">item2-2</MenuItem>
+            <SubMenu key="2-3" title="Submenu 2-3" className="list-reset">
+              <MenuItem key="2-3-1">item2-3-1</MenuItem>
+              <MenuItem key="2-3-2">item2-3-2</MenuItem>
+            </SubMenu>
+          </SubMenu>
         </Menu>
       </div>
     )
