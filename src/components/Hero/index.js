8ad28bd546d438f4eea7aa837aaef22dc5b4788c
Further Development <furtherdev1@furthers-imac.lan>
2018-03-29 16:46:53 -0700

    update hero height to vh

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import Button from '../Button'
 
 export default () => (
-   <div className="flex bg-grey-lighter">
+   <div className="flex items-center bg-grey-lighter h-screen-3/4 sm:h-screen-2/4 ">
       <div className="max-w-xl w-5/6  mx-auto p-4 sm:p-8 md:my-8">
         <h2 className="leading-loose">Further Gatsby Starter</h2>
         <p className="mb-4 leading-normal">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur quia totam ut commodi esse, excepturi consectetur harum asperiores recusandae deleniti nihil distinctio optio assumenda provident quae? Quos laudantium officiis earum.</p>
