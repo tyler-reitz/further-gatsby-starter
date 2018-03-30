99f57dbbd3488d3f421e5edd061979d0c96dd9b2
Further Development <furtherdev1@furthers-imac.lan>
2018-03-30 13:27:43 -0700

    unhack rc-menu popup menu

--- src/components/Mega/index.js
+++ src/components/Mega/index.js
@@ -3,21 +3,32 @@ import Menu, { SubMenu, Item as MenuItem, Divider } from 'rc-menu'
 
 export default () => (
   <nav id="mega" className="bg-grey-darker text-white hidden md:block">
-    <Menu mode="horizontal" className="flex justify-center list-reset leading-loose">
+    <Menu 
+      mode="horizontal"
+      className="flex justify-center list-reset leading-loose"
+      getPopupContainer={menuItemEl => menuItemEl}
+      triggerSubMenuAction="click"
+    >
       <MenuItem>Home</MenuItem>
       <MenuItem>About</MenuItem>
       <MenuItem>Contact</MenuItem>
       <SubMenu key="1" title="Submenu 1" className="bg-grey-darker text-grey-lighter">
         <MenuItem key="1-1">item1-1</MenuItem>
         <MenuItem key="1-2">item1-2</MenuItem>
+        <MenuItem key="1-3">item1-3</MenuItem>
+        <MenuItem key="1-4">item1-4</MenuItem>
+        <MenuItem key="1-5">item1-5</MenuItem>
       </SubMenu>
       <SubMenu key="2" title="Submenu 2" className="bg-grey-darker text-grey-lighter">
         <MenuItem key="2-1">item2-1</MenuItem>
         <MenuItem key="2-2">item2-2</MenuItem>
+        <MenuItem key="2-3">item2-3</MenuItem>
+        <MenuItem key="2-4">item2-4</MenuItem>
       </SubMenu>
       <SubMenu key="3" title="Submenu 3" className="bg-grey-darker text-grey-lighter">
         <MenuItem key="3-1">item3-1</MenuItem>
         <MenuItem key="3-2">item3-2</MenuItem>
+        <MenuItem key="3-3">item3-3</MenuItem>
       </SubMenu>
       <SubMenu key="4" title="Submenu 4" className="bg-grey-darker text-grey-lighter">
         <MenuItem key="4-1">item4-1</MenuItem>
---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default () => (
+  <div>
+    <h1>About Us</h1>
+  </div>
+)
