57e70263b559f044df7c7174e5c70916d20a1103
Further Development <furtherdev1@furthers-imac.lan>
2018-04-06 12:35:08 -0700

    fixes cta/phone responsive issues

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -49,7 +49,7 @@ class Header extends Component {
             >
               <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
             </svg>
-            <span className="hidden sm:block font-bold text-5xl tracking-tight">
+            <span className="hidden md:block font-bold text-5xl tracking-tight">
               Further
             </span>
           </Link>
