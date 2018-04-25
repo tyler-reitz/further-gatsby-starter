1186b32831d37acea7c99715d03e5e3598a6333e
Further Development <furtherdev1@tylers-imac.lan>
2018-04-25 14:09:27 -0700

    adds skew to Button components

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
+import styles from './style.module.css'
+
+
 export default ({ children, className, primary, ...props }) => {
+  const { left, width, button_z_index, skew_z_index } = styles
 
   return (
-    <button className={`bg-${primary ? 'primary' : 'white'} px-4 py-2 rounded font-bold text-${ primary ? 'white' : 'primary' } capitalize leading-normal ${className}`}>
+    <button className={`relative bg-${primary ? 'primary' : 'white'} px-4 py-2 rounded font-bold text-${ primary ? 'white' : 'primary' } capitalize leading-normal whitespace-no-wrap mx-4 ${className ? className : '' } ${button_z_index}`}>
       {children}
+      <div className={`absolute bg-${primary ? 'primary' : 'white'} w-full h-full pin-t pin-l skew ${left} ${width} ${skew_z_index}`}></div>
     </button>
   )
 }
